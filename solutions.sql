4d24819fe3b3a1283c449ecfc90455177de62751
Anirudh-Unni <anirudh.unni@gmail.com>
2025-08-05 16:49:36 +0200

    SQLselect-finalversion

---
+++ solutions.sql
@@ -0,0 +1,14 @@
+-- Write a query to display for each store its store ID, city, and country.
+SELECT
+	c.country_id AS "Country ID",
+	a.au_lname AS "LAST NAME",
+	a.au_fname AS "FIRST NAME",
+	t.title AS "TITLE",
+	p.pub_name AS "PUBLISHER"
+
+-- Write a query to display how much business, in dollars, each store brought in.
+-- What is the average running time of films by category?
+-- Which film categories are longest?
+-- Display the most frequently rented movies in descending order.
+-- List the top five genres in gross revenue in descending order.
+-- Is "Academy Dinosaur" available for rent from Store 1?
