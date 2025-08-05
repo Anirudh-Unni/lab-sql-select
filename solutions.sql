c778277fe40e993d0dd2ff9eebb43a8cd304dadf
Anirudh-Unni <anirudh.unni@gmail.com>
2025-08-05 17:00:03 +0200

    LabSQLselect_finalversion

--- solutions.sql
+++ solutions.sql
@@ -1,14 +1,56 @@
--- Write a query to display for each store its store ID, city, and country.
+--CHALLENGE 1
+
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    t.title AS "TITLE",
+    p.pub_name AS "PUBLISHER"
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN publishers AS p ON t.pub_id = p.pub_id;
+
+--CHALLENGE 2
+
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    p.pub_name AS "PUBLISHER",
+    COUNT(DISTINCT t.title_id) AS "TITLE COUNT"
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN publishers AS p ON t.pub_id = p.pub_id
+GROUP BY a.au_id, a.au_lname, a.au_fname, p.pub_name
+ORDER BY "TITLE COUNT" DESC;
+
+--CHALLENGE 3 
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    COUNT(s.qty) AS "TOTAL"
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN sales AS s ON t.title_id = s.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY "TOTAL" DESC LIMIT 3;
+
+
+-- Challenge 4 - Best Selling Authors Ranking
 SELECT
-	c.country_id AS "Country ID",
+	a.au_id AS "AUTHOR ID",
 	a.au_lname AS "LAST NAME",
 	a.au_fname AS "FIRST NAME",
-	t.title AS "TITLE",
-	p.pub_name AS "PUBLISHER"
-
--- Write a query to display how much business, in dollars, each store brought in.
--- What is the average running time of films by category?
--- Which film categories are longest?
--- Display the most frequently rented movies in descending order.
--- List the top five genres in gross revenue in descending order.
--- Is "Academy Dinosaur" available for rent from Store 1?
+    ifnull(SUM(sa.qty), 0) AS "TOTAL"
+FROM authors AS a
+LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
+LEFT JOIN sales AS sa on ta.title_id = sa.title_id
+group by 
+a.au_id,
+a.au_lname,
+a.au_fname
+ORDER BY "TOTAL" DESC;
